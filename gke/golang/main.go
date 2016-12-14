be71b170fbdd91b6a5e2fed36189e3a3d8ecca0e
Shintaro Kaneko <kaneshin0120@gmail.com>
2016-12-14 17:06:34 +0000

    Update gke/golang

--- gke/golang/main.go
+++ gke/golang/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, Container!")
+		fmt.Fprintf(w, "Hello world!")
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
