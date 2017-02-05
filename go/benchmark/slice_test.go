ed13010c89c52b886cfd5071c6375d34bb82c5ec
Shintaro Kaneko <kaneshin0120@gmail.com>
2017-02-05 04:14:31 +0000

    Add merge

--- go/benchmark/slice_test.go
+++ go/benchmark/slice_test.go
@@ -51,3 +51,36 @@ func BenchmarkSlice(b *testing.B) {
 		}
 	})
 }
+
+func mergeAAndB(a, b []int) []int {
+	ret := make([]int, 0, len(a)+len(b))
+	const step = 2
+
+	i := 0
+	for _, b := range b {
+		left := i * step
+		right := (i + 1) * step
+		ret = append(ret, a[left:right]...)
+		ret = append(ret, b)
+		_ = b
+		i++
+	}
+
+	return append(ret, a[i*step:]...)
+}
+
+var c []int
+
+func BenchmarkMergeAAndB(b *testing.B) {
+	as := []int{
+		1, 2, 3, 4, 5, 6, 7, 8, 9,
+	}
+	bs := []int{
+		91, 92, 93,
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		c = mergeAAndB(as, bs)
+	}
+}
