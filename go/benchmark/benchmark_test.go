6a0ef35661742911d5787d65f5aafe4e10f68790
Shintaro Kaneko <kaneshin0120@gmail.com>
2017-08-18 18:13:37 +0000

    Add benchmark

--- go/benchmark/benchmark_test.go
+++ go/benchmark/benchmark_test.go
@@ -1,14 +1,41 @@
 package benchmark
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
+	"time"
 )
 
-func BenchmarkFoo(b *testing.B) {
-	b.Run("Bar", func(b *testing.B) {
+func Test_Divide(t *testing.T) {
+	const N = 1000000
+	tmp := float64(1) / float64(time.Millisecond)
+	for j := 0; j < N; j++ {
+		f1 := int64(j) / int64(time.Millisecond)
+		f2 := int64(float64(j) * tmp)
+		if !reflect.DeepEqual(f1, f2) {
+			t.Fatal("Should be the same values")
+		}
+	}
+}
+
+var f int64
+
+func Benchmark_Divide(b *testing.B) {
+	const N = 1000000
+	b.Run("Divide", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			_ = fmt.Sprint("%d", i)
+			for j := 0; j < N; j++ {
+				f = int64(j) / int64(time.Millisecond)
+			}
+		}
+	})
+
+	b.Run("Multiple", func(b *testing.B) {
+		t := float64(1) / float64(time.Millisecond)
+		for i := 0; i < b.N; i++ {
+			for j := 0; j < N; j++ {
+				f = int64(float64(j) * t)
+			}
 		}
 	})
 }
