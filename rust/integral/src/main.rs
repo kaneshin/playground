d059aaf4f5b188c4adf1b9bdda61b87ca952b794
Shintaro Kaneko <kaneshin0120@gmail.com>
2019-12-05 00:27:22 +0900

    Apply Tuple

--- rust/integral/src/main.rs
+++ rust/integral/src/main.rs
@@ -1,11 +1,13 @@
+struct Interval(f32, f32);
+
 fn f(x: f32) -> f32 {
     return x * x - x - 1.0;
 }
 
-fn midpoint_rule(a: f32, b: f32, n: i32) -> f32 {
-    let h = (b - a) / n as f32;
+fn midpoint_rule(i: Interval, n: i32) -> f32 {
+    let h = (i.1 - i.0) / n as f32;
     let mut r = 0.0;
-    let mut x = a + h / 2.0;
+    let mut x = i.0 + h / 2.0;
 
     for _ in 0..n {
         r += f(x);
@@ -14,11 +16,11 @@ fn midpoint_rule(a: f32, b: f32, n: i32) -> f32 {
     return r * h;
 }
 
-fn trapezoid_rule(a: f32, b: f32, n: i32) -> f32 {
-    let h = (b - a) / n as f32;
+fn trapezoid_rule(i: Interval, n: i32) -> f32 {
+    let h = (i.1 - i.0) / n as f32;
     let mut r = 0.0;
-    let mut x = a + h;
-    let mut y1 = f(a);
+    let mut x = i.0 + h;
+    let mut y1 = f(i.0);
 
     for _ in 0..n {
         let y2 = f(x);
@@ -30,6 +32,6 @@ fn trapezoid_rule(a: f32, b: f32, n: i32) -> f32 {
 }
 
 fn main() {
-    println!("Midpoint Rule: {}", midpoint_rule(-2.0, 4.0, 1000));
-    println!("Trapezoid Rule: {}", trapezoid_rule(-2.0, 4.0, 1000));
+    println!("Midpoint Rule: {}", midpoint_rule(Interval(-2.0, 4.0), 1000));
+    println!("Trapezoid Rule: {}", trapezoid_rule(Interval(-2.0, 4.0), 1000));
 }
