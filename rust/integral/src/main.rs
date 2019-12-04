2f4fddd561b51ab6bc8621d899fa0403d7d80d8e
Shintaro Kaneko <kaneshin0120@gmail.com>
2019-12-05 01:35:59 +0900

    Update

--- rust/integral/src/main.rs
+++ rust/integral/src/main.rs
@@ -1,37 +1,84 @@
+trait Integral {
+    fn method(&self) -> String;
+    fn calc(&self) -> f32;
+    fn print(&self) {
+        println!("{}: {}", self.method(), self.calc());
+    }
+}
+
 struct Interval(f32, f32);
 
-fn f(x: f32) -> f32 {
-    return x * x - x - 1.0;
+struct MidpointRule {
+    function: fn(f32) -> f32,
+    interval: Interval,
+    step: i32,
 }
 
-fn midpoint_rule(i: Interval, n: i32) -> f32 {
-    let h = (i.1 - i.0) / n as f32;
-    let mut r = 0.0;
-    let mut x = i.0 + h / 2.0;
-
-    for _ in 0..n {
-        r += f(x);
-        x += h;
+impl Integral for MidpointRule {
+    fn method(&self) -> String {
+        return String::from("Midpoint Rule");
     }
-    return r * h;
+
+    fn calc(&self) -> f32 {
+        let a = self.interval.0;
+        let b = self.interval.1;
+        let n = self.step;
+        let h = (b - a) / n as f32;
+        let mut r = 0.0;
+        let mut x = a + h / 2.0;
+
+        for _ in 0..n {
+            r += (self.function)(x);
+            x += h;
+        }
+        return r * h;
+    }
 }
 
-fn trapezoid_rule(i: Interval, n: i32) -> f32 {
-    let h = (i.1 - i.0) / n as f32;
-    let mut r = 0.0;
-    let mut x = i.0 + h;
-    let mut y1 = f(i.0);
+struct TrapezoidRule {
+    function: fn(f32) -> f32,
+    interval: Interval,
+    step: i32,
+}
 
-    for _ in 0..n {
-        let y2 = f(x);
-        r += y1 + y2;
-        x += h;
-        y1 = y2;
+impl Integral for TrapezoidRule {
+    fn method(&self) -> String {
+        return String::from("Trapezoid Rule");
     }
-    return r * h / 2.0;
+
+    fn calc(&self) -> f32 {
+        let a = self.interval.0;
+        let b = self.interval.1;
+        let n = self.step;
+        let h = (b - a) / n as f32;
+        let mut r = 0.0;
+        let mut x = a + h;
+        let mut y1 = (self.function)(a);
+
+        for _ in 0..n {
+            let y2 = (self.function)(x);
+            r += y1 + y2;
+            x += h;
+            y1 = y2;
+        }
+        return r * h / 2.0;
+    }
 }
 
 fn main() {
-    println!("Midpoint Rule: {}", midpoint_rule(Interval(-2.0, 4.0), 1000));
-    println!("Trapezoid Rule: {}", trapezoid_rule(Interval(-2.0, 4.0), 1000));
+    fn f(x: f32) -> f32 {
+        return x * x - x - 1.0;
+    }
+
+    MidpointRule {
+        function: f,
+        interval: Interval(-2.0f32, 4.0f32),
+        step: 1000,
+    }.print();
+
+    TrapezoidRule {
+        function: f,
+        interval: Interval(-2.0f32, 4.0f32),
+        step: 1000,
+    }.print();
 }
